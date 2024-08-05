de1b30eb7b1dd44215a05b3798518be8adf22d35
Rraf3l <127207161+Rraf3l@users.noreply.github.com>
2024-08-05 09:54:45 -0300

    Add files via upload

---
+++ calculadoraReact-main/novoProjeto/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Frase from './components/calculadora'
+
+
+function App() {
+
+  return (
+    <>
+     <Calculadora />
+    </>
+  )
+}
+
+export default App
